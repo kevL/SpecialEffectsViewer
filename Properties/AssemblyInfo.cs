d02d0fbcdd430e62678779c8b1e1fa6a4f264a57
kevL <k3v1s73n@gmail.com>
2021-01-19 02:22:08 -0700

    update date

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("kL")]
 [assembly: AssemblyProduct("SpecialEffectsViewer")]
-[assembly: AssemblyCopyright("copyleft 2021 jan 16")]
+[assembly: AssemblyCopyright("copyleft 2021 jan 19")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
