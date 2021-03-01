3f2a898e542b9b87fd788c35516b98c9ed747914
kevL <k3v1s73n@gmail.com>
2021-02-28 19:02:23 -0700

    1.0.8.0 - update ver

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
