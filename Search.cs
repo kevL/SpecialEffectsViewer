ec2b2424cdd14c77da07478d63ed6a242ebe8d2d
kevL <k3v1s73n@gmail.com>
2021-03-03 21:14:11 -0700

    Search - fix for no effect selected AND not found

--- Search.cs
+++ Search.cs
@@ -13,10 +13,10 @@ namespace specialeffectsviewer
 		/// <summary>
 		/// Searches through the effects-list descending or ascending.
 		/// </summary>
-		/// <param name="effects"></param>
-		/// <param name="text"></param>
-		/// <param name="descend"></param>
-		/// <returns></returns>
+		/// <param name="effects">the effects-list</param>
+		/// <param name="text">the text to search for</param>
+		/// <param name="descend">true if search-descending, false if ascending</param>
+		/// <returns>the effect-id found or -1 if not found</returns>
 		internal static int SearchEffects(ListBox effects, string text, bool descend)
 		{
 			int id = -1;
@@ -41,12 +41,19 @@ namespace specialeffectsviewer
 						{
 							if (id == effects.SelectedIndex) // not found.
 							{
-								SystemSounds.Beep.Play();
+								id = -1;
 								break;
 							}
 
 							if (++id == its) // wrap to first node
+							{
+								if (effects.SelectedIndex == -1)
+								{
+									id = -1;
+									break;
+								}
 								id = 0;
+							}
 						}
 					}
 					else
@@ -62,16 +69,27 @@ namespace specialeffectsviewer
 						{
 							if (id == effects.SelectedIndex) // not found.
 							{
-								SystemSounds.Beep.Play();
+								id = -1;
 								break;
 							}
 
 							if (--id == -1) // wrap to last node
+							{
+								if (effects.SelectedIndex == -1)
+								{
+									id = -1;
+									break;
+								}
 								id = its - 1;
+							}
 						}
 					}
 				}
 			}
+
+			if (id == -1)
+				SystemSounds.Beep.Play();
+
 			return id;
 		}
 	}
